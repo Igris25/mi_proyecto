a1ec9ac342274b91feb8efbd0fb9cf086f6e3e13
Jose Alberto <js4855017@gmail.com>
2026-03-27 09:35:53 -0600

    Se agrega modelo Pago en app pagos

--- pagos/models.py
+++ pagos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pago(models.Model):
+    usuario = models.CharField(max_length=100)
+    monto = models.IntegerField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.usuario
